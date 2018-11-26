74dc93bef32c90e83d1794cff7b77eea5b277086
vdf <vdf_git@protonmail.com>
2018-11-26 13:28:39 +0100

    muh

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+package com.ns.gitcrypttest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.print("vewy vewy secwet...");
+    }
+}
